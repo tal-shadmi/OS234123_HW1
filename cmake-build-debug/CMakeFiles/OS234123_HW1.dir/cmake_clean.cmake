7aaca164fbee59fe3046cd72ba738a6ffa903ade
tal shadmi <talshadmi@campus.technion.ac.il>
2021-04-16 18:24:15 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OS234123_HW1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OS234123_HW1.dir/main.cpp.obj"
+  "OS234123_HW1.exe"
+  "OS234123_HW1.exe.manifest"
+  "OS234123_HW1.pdb"
+  "libOS234123_HW1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OS234123_HW1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
